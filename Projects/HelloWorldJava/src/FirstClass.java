ccc9ad2e554d7cedbd2354a8f6a258189792c43a
brighamwebster <brigham.webster@hoj.net>
2024-12-04 12:25:09 -0700

    push

--- Projects/HelloWorldJava/src/FirstClass.java
+++ Projects/HelloWorldJava/src/FirstClass.java
@@ -1,5 +1,16 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.print("Hello world");
+        System.out.println("Hello world");
+        boolean isAlien = false;
+        if(isAlien == true) {
+            System.out.println("It is not an Alien");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 100;
+        if (topScore > 100){
+            System.out.println("You got a highscore!");
+        }
+
     }
 }
